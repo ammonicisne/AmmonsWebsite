435e11aad7db650a83f3891e2736094e17e53106
Ammon Cisneros <ammon.15.2015.ci@gmail.com>
2019-06-25 09:49:20 -0700

    Updated About page -created styles for About apge 25June2019

--- src/components/pages/contact.js
+++ src/components/pages/contact.js
@@ -1,6 +1,9 @@
 import React from "react";
+import { library } from "@fortawesome/fontawesome-svg-core";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+library.add(faGithub, faLinkedin);
 
 export default function() {
   return (
@@ -9,6 +12,21 @@ export default function() {
       <div className="dropDown">
         <div className="iconPhone">
           <FontAwesomeIcon icon={faPhone} />
+        </div>
+        <div id="contact">
+          <div className="info">ammon cisneros</div>
+          <div className="info">801-814-9083</div>
+          <div className="info">ammon.15.2015.ci@gmail.com</div>
+          <div className="info">
+            <a href="https://github.com/ammonicisne">
+              <FontAwesomeIcon className="icons" icon={["fab", "github"]} />
+            </a>
+          </div>
+          <div className="info">
+            <a href="https://www.linkedin.com/in/ammon-cisneros/">
+              <FontAwesomeIcon className="icons" icon={["fab", "linkedin"]} />
+            </a>
+          </div>
         </div>
       </div>
     </div>
