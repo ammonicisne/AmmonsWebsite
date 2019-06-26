7ebae85c06f7c6bb8bffd6f3d4b6a0a3e8896c63
Ammon Cisneros <ammon.15.2015.ci@gmail.com>
2019-06-26 15:35:14 -0700

    Test 5

--- server.js
+++ server.js
@@ -2,16 +2,6 @@ const express = require("express");
 const port = process.env.PORT || 8080;
 const app = express();
 
-var cors_proxy = require("cors-anywhere");
-cors_proxy
-  .createServer({
-    originWhitelist: [], // Allow all origins
-    requireHeader: ["origin", "x-requested-with"],
-    removeHeaders: ["cookie", "cookie2"]
-  })
-  .listen(port, host, function() {
-    console.log("Running CORS Anywhere on " + host + ":" + port);
-  });
 app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req, res) {
   res.sendFile(__dirname + "/dist/index.html");
